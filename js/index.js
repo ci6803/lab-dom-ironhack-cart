d3a3b975502faa7c277ce5459fd03c7eb1fde0e4
ci6803 <ci6803@gmail.com>
2022-07-25 23:27:18 +0200

    solved lab

--- js/index.js
+++ js/index.js
@@ -1,24 +1,48 @@
 // ITERATION 1
+// const { product } = require('puppeteer');
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = parseInt(product.querySelector('.price span').innerText);
+  const quantity = parseInt(product.querySelector('.quantity input').value);
 
-  //... your code goes here
+  const subtotalValue = price * quantity;
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerText = `${subtotalValue}`;
+
+  return subtotalValue;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
 }
+
+// ITERATION 2
+function calculateAll() {
+  const items = document.querySelectorAll('.product');
+  items.forEach((product) => {
+    updateSubtotal(product);
+  });
+}
+
+// const products = document.getElementsByClassName('product');
+// for (let i = 0; i < products.length; i++) {
+//   updateSubtotal(products[i]);
+// }
+
+// ITERATION 3
+
+const subtotalSum = document.querySelectorAll('.subtotal span');
+
+let totalValue = 0;
+subtotalSum.forEach((subtotal) => {
+  return (totalValue += parseFloat(subtotal.innerHtml));
+});
+// console.log(totalValue);
+const finalTotal = document.querySelector('#total-value span');
+finalTotal.innerHTML = totalValue;
 
 // ITERATION 4
 
